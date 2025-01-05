0cd1dac160c67a0a12233dd97486a6861c4ec59f
ArshBakshi <arshbakshi@gmail.com>
2025-01-06 00:10:00 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MainPage from './pages/MainPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Coding Platform</h1>
+      </header>
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
